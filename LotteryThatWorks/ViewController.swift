fd83dc450e462606c26d02628f2a40f71d1c724e
Nick <117998@swcsd.us>
2021-03-15 14:09:54 -0400

    Initial Commit

---
+++ LotteryThatWorks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LotteryThatWorks
+//
+//  Created by Nicholas Centers on 3/15/21.
+//  Copyright © 2021 Nicholas Centers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
